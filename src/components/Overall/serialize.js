c39309b09ed88be2bd22e5a33ee477c27ed8c541
刘欢 <1641186065@qq.com>
2020-11-02 20:35:20 +0800

    fix

--- src/components/Overall/serialize.js
+++ src/components/Overall/serialize.js
@@ -1,27 +1,32 @@
-import _ from 'lodash'
+import _ from "lodash";
 export default {
-  data () {
+  data() {
     return {
       // 权限配置
       _authority: [],
-      jurisdictionValue: ''
-    }
+      jurisdictionValue: ""
+    };
   },
-  created () {
-    console.log(this.$route, 'serialize')
+  created() {
+    console.log(this.$route, "serialize");
     // 获取角色  初始化默认选中
-    this._authority = this.$route.meta.authority
-    this.jurisdictionValue = this._authority[0].responsibilityId
+    this._authority = this.$route.meta.authority;
+    this.jurisdictionValue = this._authority[0].responsibilityId;
   },
   computed: {
     // 职责组
-    jurisdictionOptions () {
-      return this._authority.map((e) => ({ label: e.responsibilityName, value: e.responsibilityId }))
+    jurisdictionOptions() {
+      return this._authority.map(e => ({
+        label: e.responsibilityName,
+        value: e.responsibilityId
+      }));
     },
     // 按钮组
-    jurisdictionButs () {
-      let maps = this._authority.filter((e) => e.responsibilityId === this.jurisdictionValue)
-      return maps.length ? _.cloneDeep(maps[0].map) : []
+    jurisdictionButs() {
+      let maps = this._authority.filter(
+        e => e.responsibilityId === this.jurisdictionValue
+      );
+      return maps.length ? _.cloneDeep(maps[0].map) : [];
     }
   }
-}
+};
