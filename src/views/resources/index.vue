1d93d8b6d580bcdbfff533657f6d6094deef8d3d
刘欢 <1641186065@qq.com>
2020-12-25 17:34:03 +0800

    update

---
+++ src/views/resources/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="resources">
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="explain" label="展示说明"> </el-table-column>
+      <el-table-column prop="type" label="类型" width="180"> </el-table-column>
+      <el-table-column prop="status" label="是否失效"> </el-table-column>
+      <el-table-column prop="des" label="功能描述"> </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Resources",
+  data() {
+    return {
+      tableData: []
+    };
+  },
+  components: {}
+};
+</script>
