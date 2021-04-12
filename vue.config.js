d535e45ae003de1620f4c9044066c3baab71d72f
刘欢 <1641186065@qq.com>
2021-04-12 17:22:25 +0800

    简化梳理

--- vue.config.js
+++ vue.config.js
@@ -17,13 +17,6 @@ const port = process.env.port || process.env.npm_config_port || 9527; // dev por
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
-  /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
-   */
   publicPath: "/",
   outputDir: "dist",
   assetsDir: "static",
