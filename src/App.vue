b559fe1a19298b3234a196be8f0eee185c4535e6
huan0118 <1641186065@qq.com>
2021-05-06 23:00:19 +0800

    404

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+#app {
+  height: 100%;
+}
+</style>
--- src/views/error-page/404.vue
+++ src/views/error-page/404.vue
@@ -1,214 +1,97 @@
 <template>
-  <div class="wscn-http404-container">
-    <div class="wscn-http404">
-      <div class="pic-404">
-        404
-      </div>
+  <section class="Page404">
+    <div class="cube-grid">
+      <div class="cube cube-1">4</div>
+      <div class="cube cube-2">0</div>
+      <div class="cube cube-3">4</div>
+      <div class="cube cube-4">0</div>
+      <div class="cube cube-5">0</div>
+      <div class="cube cube-6">0</div>
+      <div class="cube cube-7">4</div>
+      <div class="cube cube-8">0</div>
+      <div class="cube cube-9">4</div>
     </div>
-  </div>
+
+    <div class="back">
+      <a href="/" rel="noopener noreferrer">返回首页</a>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "Page404",
-  computed: {
-    message() {
-      return "The webmaster said that you can not enter this page...";
-    }
-  }
+  name: "Page404"
 };
 </script>
 
 <style lang="scss" scoped>
-.wscn-http404-container {
-  transform: translate(-50%, -50%);
-  position: absolute;
-  top: 40%;
-  left: 50%;
-}
-.wscn-http404 {
-  position: relative;
-  width: 1200px;
-  padding: 0 50px;
-  overflow: hidden;
-  .pic-404 {
-    position: relative;
-    float: left;
-    width: 600px;
-    overflow: hidden;
-    &__parent {
-      width: 100%;
-    }
-    &__child {
-      position: absolute;
-      &.left {
-        width: 80px;
-        top: 17px;
-        left: 220px;
-        opacity: 0;
-        animation-name: cloudLeft;
-        animation-duration: 2s;
-        animation-timing-function: linear;
-        animation-fill-mode: forwards;
-        animation-delay: 1s;
-      }
-      &.mid {
-        width: 46px;
-        top: 10px;
-        left: 420px;
-        opacity: 0;
-        animation-name: cloudMid;
-        animation-duration: 2s;
-        animation-timing-function: linear;
-        animation-fill-mode: forwards;
-        animation-delay: 1.2s;
-      }
-      &.right {
-        width: 62px;
-        top: 100px;
-        left: 500px;
-        opacity: 0;
-        animation-name: cloudRight;
-        animation-duration: 2s;
-        animation-timing-function: linear;
-        animation-fill-mode: forwards;
-        animation-delay: 1s;
-      }
-      @keyframes cloudLeft {
-        0% {
-          top: 17px;
-          left: 220px;
-          opacity: 0;
-        }
-        20% {
-          top: 33px;
-          left: 188px;
-          opacity: 1;
-        }
-        80% {
-          top: 81px;
-          left: 92px;
-          opacity: 1;
-        }
-        100% {
-          top: 97px;
-          left: 60px;
-          opacity: 0;
-        }
-      }
-      @keyframes cloudMid {
-        0% {
-          top: 10px;
-          left: 420px;
-          opacity: 0;
-        }
-        20% {
-          top: 40px;
-          left: 360px;
-          opacity: 1;
-        }
-        70% {
-          top: 130px;
-          left: 180px;
-          opacity: 1;
-        }
-        100% {
-          top: 160px;
-          left: 120px;
-          opacity: 0;
-        }
-      }
-      @keyframes cloudRight {
-        0% {
-          top: 100px;
-          left: 500px;
-          opacity: 0;
-        }
-        20% {
-          top: 120px;
-          left: 460px;
-          opacity: 1;
-        }
-        80% {
-          top: 180px;
-          left: 340px;
-          opacity: 1;
-        }
-        100% {
-          top: 200px;
-          left: 300px;
-          opacity: 0;
-        }
-      }
-    }
+.Page404 {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 14px;
+
+  .back {
+    margin-top: 10px;
+    font-size: 16px;
   }
-  .bullshit {
-    position: relative;
+
+  .cube-grid {
+    width: 128px;
+    height: 128px;
+    color: white;
+  }
+  .cube-grid .cube {
+    width: 33%;
+    height: 33%;
+    background-color: #126e82;
     float: left;
-    width: 300px;
-    padding: 30px 0;
-    overflow: hidden;
-    &__oops {
-      font-size: 32px;
-      font-weight: bold;
-      line-height: 40px;
-      color: #1482f0;
-      opacity: 0;
-      margin-bottom: 20px;
-      animation-name: slideUp;
-      animation-duration: 0.5s;
-      animation-fill-mode: forwards;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: cube-grid 1.3s infinite ease-in-out;
+  }
+  .cube-grid .cube-1 {
+    animation-delay: 0.2s;
+  }
+  .cube-grid .cube-2 {
+    animation-delay: 0.3s;
+  }
+  .cube-grid .cube-3 {
+    animation-delay: 0.4s;
+  }
+  .cube-grid .cube-4 {
+    animation-delay: 0.1s;
+  }
+  .cube-grid .cube-5 {
+    animation-delay: 0.2s;
+  }
+  .cube-grid .cube-6 {
+    animation-delay: 0.3s;
+  }
+  .cube-grid .cube-7 {
+    animation-delay: 0s;
+  }
+  .cube-grid .cube-8 {
+    animation-delay: 0.1s;
+  }
+  .cube-grid .cube-9 {
+    animation-delay: 0.2s;
+  }
+  @keyframes cube-grid {
+    0% {
+      transform: scale3D(1, 1, 1);
     }
-    &__headline {
-      font-size: 20px;
-      line-height: 24px;
-      color: #222;
-      font-weight: bold;
-      opacity: 0;
-      margin-bottom: 10px;
-      animation-name: slideUp;
-      animation-duration: 0.5s;
-      animation-delay: 0.1s;
-      animation-fill-mode: forwards;
+    35% {
+      transform: scale3D(0, 0, 1);
     }
-    &__info {
-      font-size: 13px;
-      line-height: 21px;
-      color: grey;
-      opacity: 0;
-      margin-bottom: 30px;
-      animation-name: slideUp;
-      animation-duration: 0.5s;
-      animation-delay: 0.2s;
-      animation-fill-mode: forwards;
+    70% {
+      transform: scale3D(1, 1, 1);
     }
-    &__return-home {
-      display: block;
-      float: left;
-      width: 110px;
-      height: 36px;
-      background: #1482f0;
-      border-radius: 100px;
-      text-align: center;
-      color: #ffffff;
-      opacity: 0;
-      font-size: 14px;
-      line-height: 36px;
-      cursor: pointer;
-      animation-name: slideUp;
-      animation-duration: 0.5s;
-      animation-delay: 0.3s;
-      animation-fill-mode: forwards;
-    }
-    @keyframes slideUp {
-      0% {
-        transform: translateY(60px);
-        opacity: 0;
-      }
-      100% {
-        transform: translateY(0);
-        opacity: 1;
-      }
+    100% {
+      transform: scale3D(1, 1, 1);
     }
   }
 }
